8b49521f06ccebf3d5cdb8ace52f87dbcd9e9cdb
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-08-08 03:41:28 +0100

    Added lein application framework. Fixed files and .gitignore.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-mem-pwds
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_mem_pwds/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-mem-pwds.core-test
+  (:require [clojure.test :refer :all]
+            [clj-mem-pwds.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
