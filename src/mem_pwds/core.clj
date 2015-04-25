19cc99114465a3956b3fab8bda4d56db3e6143af
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-04-24 21:13:00 -0500

    Build first test and (supporting) function.

--- src/mem_pwds/core.clj
+++ src/mem_pwds/core.clj
@@ -5,3 +5,6 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn read-content [file-name]
+  (slurp file-name))
--- test/mem_pwds/core_test.clj
+++ test/mem_pwds/core_test.clj
@@ -1,7 +1,24 @@
 (ns mem-pwds.core-test
   (:require [clojure.test :refer :all]
-            [mem-pwds.core :refer :all]))
+            [mem-pwds.core :refer :all])
+  (:import [java.nio.file Files LinkOption Paths]
+           [java.nio.file.attribute FileAttribute]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest reading-test
+  (testing "read word source"
+    (let [test-dir-name ".tmp"
+          test-dir-path (Paths/get "." (into-array [test-dir-name]))
+          test-file-name "source.txt"
+          test-file-path (Paths/get (.toString test-dir-path)
+                                   (into-array [test-file-name]))
+          words ["alpha" "mike" "zulu"]
+          contents (clojure.string/join \newline words)
+          ]
+      (when (not (Files/exists test-dir-path 
+                               (into-array LinkOption [])))
+        (Files/createDirectory test-dir-path
+                             (into-array FileAttribute [])))
+      (spit (.toString test-file-path) contents)
+      (let [actual-contents
+            (mem-pwds.core/read-content (.toString test-file-path))]
+        (is (= contents actual-contents))))))
