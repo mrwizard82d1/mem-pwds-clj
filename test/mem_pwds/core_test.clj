46daffc1384cb96bde30806ecfefb572136fbe8b
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-04-24 21:24:44 -0500

    Refactor test code.
    Extract test setup (creating the test file to read with the needed
    contents) into method.

--- test/mem_pwds/core_test.clj
+++ test/mem_pwds/core_test.clj
@@ -4,21 +4,33 @@
   (:import [java.nio.file Files LinkOption Paths]
            [java.nio.file.attribute FileAttribute]))
 
+
+(defn- create-test-file [test-dir-name test-file-name contents]
+  (let [test-dir-path (Paths/get "." (into-array [test-dir-name]))
+        test-file-path (Paths/get (.toString test-dir-path)
+                                   (into-array [test-file-name]))]
+    (when (not (Files/exists test-dir-path 
+                           (into-array LinkOption [])))
+    (Files/createDirectory test-dir-path
+                           (into-array FileAttribute [])))
+    (spit (.toString test-file-path) contents)
+    test-file-path))
+
+
+
 (deftest reading-test
   (testing "read word source"
     (let [test-dir-name ".tmp"
-          test-dir-path (Paths/get "." (into-array [test-dir-name]))
           test-file-name "source.txt"
-          test-file-path (Paths/get (.toString test-dir-path)
-                                   (into-array [test-file-name]))
           words ["alpha" "mike" "zulu"]
           contents (clojure.string/join \newline words)
-          ]
-      (when (not (Files/exists test-dir-path 
-                               (into-array LinkOption [])))
-        (Files/createDirectory test-dir-path
-                             (into-array FileAttribute [])))
-      (spit (.toString test-file-path) contents)
+          test-file-path (create-test-file test-dir-name
+                                           test-file-name contents)]
       (let [actual-contents
             (mem-pwds.core/read-content (.toString test-file-path))]
         (is (= contents actual-contents))))))
+
+
+
+  
+  
