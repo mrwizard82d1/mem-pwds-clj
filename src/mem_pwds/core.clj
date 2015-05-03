4d016634cf3e395e2028f3d6a2ded3ce7060cb88
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-05-03 00:29:14 -0500

    Add next-password/0 function.
    This function, since we pass it no information, creates a source of
    words by reading from "dict.txt."

--- src/mem_pwds/core.clj
+++ src/mem_pwds/core.clj
@@ -9,11 +9,19 @@
   (println "Hello, World!"))
 
 
-(defn read-content [file-name]
-  (slurp file-name))
+(defn read-content
+  ([file-name]
+   (slurp file-name)))
+
+
+(def content (atom nil))
 
 
 (defn next-password
+  ([] (if (not @content)
+        (swap! content (fn [_old]
+                         (str/split (read-content (.getFile (clojure.java.io/resource "dict.txt"))) #"\s+"))))
+    (next-password @content))
   ([words] (next-password words rand-nth (str/join (map char (range 33 64))) rand-nth))
   ([words words-f chars chars-f]
    (str (words-f words) (chars-f chars) (words-f words))))
